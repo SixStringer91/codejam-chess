9a39c0705a12ba02760793d743ba0bad670b7d29
SixStringer91 <danlootsev@gmail.com>
2021-07-20 02:17:23 +0300

    feat: add alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
+import './App.scss';
 import Header from './components/header/header';
 import Main from './components/main/main';
 import Footer from './components/footer/footer';
 import Popup from './components/popup/popup';
 
 function App() {
+  alert('Привет. шахматы в процессе производства ещё. прошу не проверять работу пару дней, хочу хоть какой то функционал доделать. спасибо!');
   return (
     <>
       <Popup />
